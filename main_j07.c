aec22f673e532fcb82709ba7d88aba7c0d927084
Johan BYTTNER <jbyttner@e2r11p8.42.fr>
2015-09-10 16:06:34 +0200

    Harmonising main.j07.c

--- main_j07.c
+++ main_j07.c
@@ -1,27 +1,47 @@
-/ ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_j07.c                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbyttner <jbyttner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ecousine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/09/10 15:41:26 by jbyttner          #+#    #+#             */
-/*   Updated: 2015/09/10 15:42:05 by jbyttner         ###   ########.fr       */
+/*   Created: 2015/09/10 09:47:53 by ecousine          #+#    #+#             */
+/*   Updated: 2015/09/10 16:05:45 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <stdio.h>
 
 char	*ft_strdup(char *src);
 
-int		main(void)
+int main(void)//(int argc, char **argv)
 {
-	char *src;
-	char *copy;
+	char	*exNO = "\n=== Ex\%02d ===\n";
+	printf(exNO, 0);
+	printf("%s", ft_strdup("a string to copy\n"));
 
-	src = "Hello World!";
-	copy = ft_strdup(src);
-	printf("\n%d src, %d copy, %s str value, %s copy value\n", src, copy, src, copy);
-	return (0);
+/*	printf("=== Ex01 ===\n");
+	int i = 0;
+	int *tab_range = ft_range(-5, 8);
+	while (i < 13)
+	{
+		printf("%d\n", tab_range[i]);
+		i++;
+	}
+	printf("\n");
+
+	printf("=== Ex02 ===\n");
+	int *a_tab;
+	int size;
+	i = 0;
+	a_tab = NULL;
+	size = ft_ultimate_range(&a_tab, 1, 4);
+	while (i < 3)
+	{
+		printf("%d\n", a_tab[i]);
+		i++;
+	}
+
+	printf("=== Ex03 ===\n");
+	printf("%s", ft_concat_params(argc, argv));*/
 }
